cc4551e65189f3ba161dc5e6a99ae7bbb7d0d60d
sorz <orz@sorz.org>
2015-01-08 15:00:38 +0800

    Initial Express project

---
+++ app/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var morgan = require('morgan');
+app.use(morgan('short'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended:false }));
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8000, function() {
+    console.log('Server running at http://localhost:8000/');
+});
