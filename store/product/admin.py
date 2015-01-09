51fb936e003a9021e87fbf28530072a7e6056a12
sorz <orz@sorz.org>
2015-01-09 22:29:10 +0800

    Add simple product models
    Category, product and rating

--- store/product/admin.py
+++ store/product/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Product, Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price')
+admin.site.register(Product, ProductAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Category, CategoryAdmin)
--- store/product/models.py
+++ store/product/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Category Name', max_length=244)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField('Product Name', max_length=255)
+    price = models.DecimalField(max_digits=9, decimal_places=2,
+                                validators=[MinValueValidator(0)])
+    category = models.ForeignKey(Category, null=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    point = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
