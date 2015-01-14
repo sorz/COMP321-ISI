2123e7c78d8c4f29490e14c927ebd1e19a6f1bff
sorz <orz@sorz.org>
2015-01-15 01:17:51 +0800

    Add shopping cart models

--- store/cart/models.py
+++ store/cart/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from product.models import Product
+
+
+class Cart(models.Model):
+    owner = models.ForeignKey(User)
+    purchased = models.BooleanField(default=False)
+
+    def purchase(self):
+        """Save prices of items and set purchased to true."""
+        assert not self.purchased
+        self.purchased = True
+
+        # Save current prices (purchase prices) permanently.
+        for item in self.cartitem_set.all():
+            assert item.quantity >= 0
+            item.purchase_price = item.product.price
+            item.save()
+
+        # TODO: create and return a order
+
+    def add_item(self, product, quantity=1):
+        """Add (or increase the quantity if existed) a product into shopping cart."""
+        assert quantity > 0
+        item, created = self.cartitem_set.get_or_create(product=product, defaults={'quantity': 0})
+        item.quantity += quantity
+        item.save()
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart)
+    product = models.ForeignKey(Product)
+    purchase_price = models.DecimalField(max_digits=9, decimal_places=2, null=True)
+    quantity = models.IntegerField(default=1)
+
+    def _get_price(self):
+        """Return purchase price after purchased or return current price."""
+        if self.purchase_price is None:
+            return self.product.price
+        else:
+            return self.purchase_price
+    price = property(_get_price)
+
+    def __str__(self):
+        return "%s x %s" % (self.product, self.quantity)
---
+++ store/cart/utils.py
@@ -0,0 +1,9 @@
+from .models import Cart
+
+
+def get_or_create_cart(request):
+    """Return user's shopping cart object if authenticated or None."""
+    if not request.user.is_authenticated():
+        return
+    cart, created = Cart.objects.get_or_create(owner=request.user, purchased=False)
+    return cart
