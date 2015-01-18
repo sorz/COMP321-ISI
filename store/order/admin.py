75adafa1c148e16e6379ba6ec6290967994f5bb3
sorz <orz@sorz.org>
2015-01-18 22:55:02 +0800

    Order admin: combine order state
    orz

--- store/order/admin.py
+++ store/order/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import OrderItem, PendingOrder, HoldingOrder, \
-    OnDeliveryOrder, ConfirmedOrder, CancelledOrder
+from .models import OrderItem, Order
 
 
 class OrderItemInline(admin.TabularInline):
@@ -18,22 +17,29 @@ class OrderItemInline(admin.TabularInline):
         return False
 
 
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('owner', 'purchase_date', 'recipient_name', 'state',
                        'shipment_date', 'recipient_address', 'recipient_address_2',
                        'recipient_postcode', 'total_price')
     fieldsets = (
         (None, {
-            'fields': ('owner', 'state', 'total_price', 'purchase_date', 'shipment_date')
+            'fields': ('owner', 'state', 'total_price',
+                       'purchase_date', 'shipment_date')
         }),
         ('Recipient Information', {
             'fields': ('recipient_name', 'recipient_postcode',
                        'recipient_address', 'recipient_address_2')
         })
     )
-    list_display = ('owner', 'recipient_name', 'purchase_date', 'state', 'total_price')
+    list_display = ('id', 'owner', 'recipient_name', 'purchase_date',
+                    'state', 'total_price')
     list_filter = ('state', )
+    search_fields = ('id', 'owner__username', 'recipient_name',
+                     'recipient_address', 'recipient_address_2')
+    date_hierarchy = 'purchase_date'
     inlines = [OrderItemInline]
+    actions = ['make_ship', 'make_hold', 'make_cancel']
 
     def has_add_permission(self, request):
         return False
@@ -56,47 +62,3 @@ class OrderAdmin(admin.ModelAdmin):
             order.cancel()
     make_cancel.short_description = 'Cancel selected order.'
 
-
-@admin.register(PendingOrder)
-class PendingOrderAdmin(OrderAdmin):
-    actions = ['make_ship', 'make_hold', 'make_cancel']
-    list_filter = []
-
-    def queryset(self, request):
-        return self.model.objects.filter(state='P')
-
-@admin.register(HoldingOrder)
-class HoldingOrderAdmin(OrderAdmin):
-    actions = ['make_ship', 'make_cancel']
-    list_filter = []
-
-    def queryset(self, request):
-        return self.model.objects.filter(state='H')
-
-
-@admin.register(OnDeliveryOrder)
-class OnDeliveryOrderAdmin(OrderAdmin):
-    list_filter = []
-
-    def queryset(self, request):
-        return self.model.objects.filter(state='S')
-
-    def get_actions(self, request):
-        return ['make_ship']
-
-@admin.register(ConfirmedOrder)
-class ConfirmedOrderAdmin(OrderAdmin):
-    actions = []
-    list_filter = []
-
-    def queryset(self, request):
-        return self.model.objects.filter(state='R')
-
-
-@admin.register(CancelledOrder)
-class CancelledOrderAdmin(OrderAdmin):
-    actions = []
-    list_filter = []
-
-    def queryset(self, request):
-        return self.model.objects.filter(state='C')
