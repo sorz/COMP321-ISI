de6c5528b2d9ff42d61fcd424b23ca58b484f305
sorz <orz@sorz.org>
2015-02-05 18:18:27 +0800

    Dashboard: upload product photos

--- store/product_dash/forms.py
+++ store/product_dash/forms.py
@@ -1,11 +1,18 @@
 from django import forms
+from django.forms.models import inlineformset_factory
 from django.forms import ModelForm
 
-
-from product.models import Product
+from product.models import Product, Photo
 
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
 
+
+class PhotoForm(ModelForm):
+    class Meta:
+        model = Photo
+
+
+PhotoFormSet = inlineformset_factory(Product, Photo, form=PhotoForm)
--- store/product_dash/views.py
+++ store/product_dash/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 
 
-from .forms import ProductForm
+from .forms import ProductForm, PhotoFormSet
 from product.models import Product
 
 
@@ -11,16 +11,20 @@ def detail(request, product_id):
 
     if request.method == 'POST':
         product_form = ProductForm(request.POST, instance=product)
+        photo_formset = PhotoFormSet(request.POST, request.FILES, instance=product)
 
-        if product_form.is_valid():
+        if product_form.is_valid() and photo_formset.is_valid():
             product_form.save()
+            photo_formset.save()
             messages.add_message(request, messages.SUCCESS, "Product saved.")
             return redirect('dashboard:category:detail', product.category_id)
 
     else:
         product_form = ProductForm(instance=product)
+        photo_formset = PhotoFormSet(instance=product)
 
-    dictionary = {'product': product, 'product_form': product_form}
+    dictionary = {'product': product, 'product_form': product_form,
+                  'photo_formset': photo_formset}
     return render(request, 'product_dash/detail.html', dictionary)
 
 
