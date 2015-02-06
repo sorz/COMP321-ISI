cb258d75b2d803ebe96be9a1d346948cbdebcb4f
sorz <orz@sorz.org>
2015-02-06 16:39:54 +0800

    Template: improve category index

--- store/category/models.py
+++ store/category/models.py
@@ -6,5 +6,18 @@ class Category(models.Model):
     name = models.CharField('Category Name', max_length=255)
     description = models.TextField(null=True)
 
+    def get_cover_image(self):
+        """
+        Return the front cover image, which is selected from
+        one of product photo in this category.
+
+        May return None if no front cover.
+        """
+        from product.models import Photo
+
+        photos = Photo.objects.filter(product__category__id=self.pk)
+        if photos.exists():
+            return photos[0].image
+
     def __str__(self):
         return self.name
