270e1b29c8805e3eec9f819e3f10cd687e93b1dc
sorz <orz@sorz.org>
2015-03-24 23:07:07 +0800

    Vendor: set product status (buggy)
    Cannot show current status (always 'In stock')

--- store/product_admin/forms.py
+++ store/product_admin/forms.py
@@ -1,7 +1,16 @@
-from django.forms.models import inlineformset_factory
+from django import forms
 from django.forms import ModelForm
+from django.forms.models import inlineformset_factory
+from sorl.thumbnail.admin.current import AdminImageWidget
 
 from product.models import Product, Photo
+
+
+PRODUCT_STATUS = (
+    ('NORMAL', 'In stock'),
+    ('OUT-OFF-STOCK', 'Out of stock'),
+    ('OFF-SHELF', 'Off shelf')
+)
 
 
 class ProductForm(ModelForm):
@@ -9,11 +18,25 @@ class ProductForm(ModelForm):
         model = Product
         exclude = ['sale_quantity', 'sale_amount', 'in_stock', 'off_shelf']
 
+    status = forms.ChoiceField(choices=PRODUCT_STATUS, initial=PRODUCT_STATUS[0])
+
+    def save(self, commit=True):
+        product = super().save(commit=False)
+
+        status = self.cleaned_data['status']
+        product.in_stock = status != 'OUT-OFF-STOCK'
+        product.off_shelf = status == 'OFF-SHELF'
+
+        if commit:
+            product.save()
+        return product
+
 
 class PhotoForm(ModelForm):
     class Meta:
         model = Photo
         fields = ['image', 'description']
+        widgets = {'image': AdminImageWidget}
 
 
 PhotoFormSet = inlineformset_factory(Product, Photo, form=PhotoForm)
