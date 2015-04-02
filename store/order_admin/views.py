351de00b057983b0a084170bbb14ad9780f18b0e
Tim <zrccxyb62@gmail.com>
2015-04-02 18:02:12 +0800

    Date validation in fulfilled & cancelled order report

--- store/order_admin/views.py
+++ store/order_admin/views.py
@@ -4,6 +4,8 @@ from admin.decorators import vendor_required
 
 from order.views import BaseOrderListView, BaseOrderDetailView
 
+from time import strptime
+from django.contrib import messages
 
 class _VendorOrderListView(BaseOrderListView):
     """Common view used by pending and on-delivery order list."""
@@ -17,6 +19,23 @@ class _VendorOrderListView(BaseOrderListView):
 class _ReportListView(_VendorOrderListView):
     """Common view used by fulfilled, cancelled and best-selling report."""
 
+    def is_valid_duration(self, date_start="", date_end=""):
+        """Determine if two given strings are valid date or duration"""
+        if len(date_start) != 0 and len(date_end) != 0:
+            try:
+                sdate = strptime(date_start, "%Y-%m-%d")
+                edate = strptime(date_end, "%Y-%m-%d")
+            except:
+                return 'ERRFMT'
+            if edate >= sdate:
+                return 'APTDATE'
+            else:
+                return 'ERRDUR'
+        elif len(date_start) == 0 and len(date_end) == 0:
+            return 'EMPDATE'
+        else:
+            return 'APTDATE'
+
     def get_queryset(self):
         orders = super().get_queryset()
         start = self.request.GET.get('start')
@@ -27,15 +46,30 @@ class _ReportListView(_VendorOrderListView):
             if end:
                 orders = orders.filter(purchase_date__lt="%s 23:59:59.99" % end)
         except ValidationError:
-            pass  # TODO: show a error message to user.
+            messages.add_message(self.request, messages.ERROR,
+                             "Date validation errs. "
+                             "Please select appropriate dates.")
         return orders
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # TODO: validate date format.
         context['start'] = self.request.GET.get('start', '')
         context['end'] = self.request.GET.get('end', '')
+        #validate date format
+        if self.is_valid_duration(context['start'], context['end']) == 'EMPDATE':
+            messages.add_message(self.request, messages.WARNING,
+                             "Date field is empty. "
+                             "Please select appropriate dates.")
+        elif self.is_valid_duration(context['start'], context['end']) == 'ERRFMT':
+            messages.add_message(self.request, messages.ERROR,
+                             "Incorrect date format. "
+                             "Please select appropriate dates.")
+        elif self.is_valid_duration(context['start'], context['end']) == 'ERRDUR':
+            messages.add_message(self.request, messages.ERROR,
+                             "End date should be later than start date. "
+                             "Please select appropriate dates.")
         return context
+
 
 
 class PendingView(_VendorOrderListView):
