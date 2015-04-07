32aa64b4a2df11237fb0bacbcc788a3903bdb150
sorz <orz@sorz.org>
2015-04-07 21:49:59 +0800

    Auto redirect to vendor register page

--- store/account_admin/urls.py
+++ store/account_admin/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, url
-from django.contrib.auth import views as auth_views
 
 from account_admin import views
 from .forms import VendorLoginForm
@@ -7,7 +6,7 @@ from .forms import VendorLoginForm
 
 urlpatterns = patterns(
     '',
-    url(r'^login/$', auth_views.login, name='login',
+    url(r'^login/$', views.login, name='login',
         kwargs={'template_name': 'account_admin/login.html',
                 'authentication_form': VendorLoginForm}),
     url(r'^register/$', views.register, name='register'),
--- store/account_admin/views.py
+++ store/account_admin/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth.views import login as login_view
 from django.db.models import Q
 
 from .forms import VendorRegistrationForm
+
+
+def _vendor_account_exists():
+    return User.objects.filter(Q(is_staff=True) | Q(is_superuser=True)).exists()
+
+
+def login(request, **kwargs):
+    if not _vendor_account_exists():
+        return redirect('admin:account:register')
+    else:
+        return login_view(request, **kwargs)
 
 
 def register(request):
@@ -11,7 +23,7 @@ def register(request):
 
     Forbidden if any staff or superuser exists.
     """
-    if User.objects.filter(Q(is_staff=True) | Q(is_superuser=True)).exists():
+    if _vendor_account_exists():
         return render(request, 'account_admin/create.html', status=403)
 
     if request.method == 'POST':
